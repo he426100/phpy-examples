046b4a411dd0d5dc0b33a20832f1d1938b00b424
弄月 <{ID}+{username}@users.noreply.github.com>
2023-12-15 12:03:07 +0800

    增加模型下载工具

---
+++ utils/download.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @link https://modelscope.cn/docs/%E6%A8%A1%E5%9E%8B%E7%9A%84%E4%B8%8B%E8%BD%BD
+ */
+require __DIR__ . '/../bootstrap.php';
+
+$snapshot_download = PyCore::import('modelscope.hub.snapshot_download')->snapshot_download;
+echo $snapshot_download($argv[1], cache_dir: getenv('MODEL_PATH') ?: null), PHP_EOL;
